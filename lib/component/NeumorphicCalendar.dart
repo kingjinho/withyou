74845835e52a50b3cfc0ecb687dd653d14a37804
brent <jinhoshin319@gmail.com>
2020-09-17 20:29:30 +0900

    changes in variables' names and etc

--- lib/component/NeumorphicCalendar.dart
+++ lib/component/NeumorphicCalendar.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:withyou/shared/Colors.dart';
 
 class NeumorphicCalendar extends StatelessWidget {
-  NeumorphicCalendar(this.width, this.height, this.backgroundColor,
-      this.headerText, this.contentText, this.headerWeight, this.contentWeight);
+  NeumorphicCalendar(
+      this.width,
+      this.height,
+      this.headerBackgroundColor,
+      this.contentBackgroundColor,
+      this.headerText,
+      this.contentText,
+      this.headerWeight,
+      this.contentWeight,
+      this.borderRadius);
 
   final double width;
   final double height;
-  final Color backgroundColor;
+  final Color headerBackgroundColor;
+  final Color contentBackgroundColor;
   final Text headerText;
   final int headerWeight;
   final Text contentText;
   final int contentWeight;
+  final double borderRadius;
 
   @override
   Widget build(BuildContext context) {
@@ -20,38 +29,47 @@ class NeumorphicCalendar extends StatelessWidget {
       height: height,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Flexible(
-              flex: headerWeight,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: colorNeuPink,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30))),
-                alignment: Alignment.center,
-                child: headerText,
-              )),
-          Flexible(
-              flex: contentWeight,
-              child: Container(
-                decoration: BoxDecoration(
-                    color: colorNeuWhite,
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey[500],
-                          offset: Offset(10.0, 10.0),
-                          blurRadius: 15.0,
-                          spreadRadius: 1.0),
-                    ],
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(30),
-                        bottomRight: Radius.circular(30))),
-                alignment: Alignment.center,
-                child: contentText,
-              )),
-        ],
+        children: _createCalendarView(),
       ),
     );
+  }
+
+  List<Widget> _createCalendarView() {
+    List<Widget> list = List();
+    if (headerWeight != 0) {
+      list.add(Flexible(
+          flex: headerWeight,
+          child: Container(
+            decoration: BoxDecoration(
+                color: headerBackgroundColor,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(borderRadius),
+                    topRight: Radius.circular(borderRadius))),
+            alignment: Alignment.center,
+            child: headerText,
+          )));
+    }
+    list.add(Flexible(
+        flex: contentWeight,
+        child: Container(
+          decoration: BoxDecoration(
+              color: contentBackgroundColor,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[500],
+                    offset: Offset(10.0, 10.0),
+                    blurRadius: 15.0,
+                    spreadRadius: 1.0),
+              ],
+              borderRadius: headerWeight != 0
+                  ? BorderRadius.only(
+                      bottomLeft: Radius.circular(borderRadius),
+                      bottomRight: Radius.circular(borderRadius))
+                  : BorderRadius.all(Radius.circular(borderRadius))),
+          alignment: Alignment.center,
+          child: contentText,
+        )));
+
+    return list;
   }
 }
